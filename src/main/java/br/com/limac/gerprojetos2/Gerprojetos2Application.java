5c7e117c8de17b9f8e9817a8195b7ac9ad82c633
walmirlsj1 <walmir_004@hotmail.com>
2023-01-22 20:03:31 -0400

    first commit

---
+++ src/main/java/br/com/limac/gerprojetos2/Gerprojetos2Application.java
@@ -0,0 +1,13 @@
+package br.com.limac.gerprojetos2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gerprojetos2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Gerprojetos2Application.class, args);
+    }
+
+}
