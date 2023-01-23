544a7ceadd76f35309f1453f9713fb9f0ae002d8
walmirlsj1 <walmir_004@hotmail.com>
2023-01-22 20:05:42 -0400

    ajustar timezone padrao

--- src/main/java/br/com/limac/gerprojetos2/Gerprojetos2Application.java
+++ src/main/java/br/com/limac/gerprojetos2/Gerprojetos2Application.java
@@ -2,11 +2,16 @@ package br.com.limac.gerprojetos2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import java.util.TimeZone;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class Gerprojetos2Application {
 
     public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
         SpringApplication.run(Gerprojetos2Application.class, args);
     }
 
